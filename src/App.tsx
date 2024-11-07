a840ffa3748346a50f56b6ef2d5fadbc591e5fc5
hyuni106 <suhyuni106@hanmail.net>
2024-11-07 17:10:20 +0900

    react-router-dom 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
+import { Route, Routes, Link } from "react-router-dom";
+
 import Circle from "components/Circle";
-import React from "react";
 
 function App() {
   return (
     <div className="App">
-      <Circle />
+      <nav>
+        <Link to="/circle">Circle Progress</Link>
+        <Link to="/line">Line Chart</Link>
+      </nav>
+      <Routes>
+        <Route path="/circle" element={<Circle />} />
+      </Routes>
     </div>
   );
 }
